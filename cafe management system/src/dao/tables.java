ac966553cccc6f3a9a2d9926d536b8861f5ece6e
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2022-09-03 20:29:57 +0530

    Update tables.java

--- cafe management system/src/dao/tables.java
+++ cafe management system/src/dao/tables.java
@@ -13,16 +13,9 @@ public class tables {
     public static void main(String[]args)
     {
         try{
-            String userTable="create table user(id int Auto_INCREMENT primary key,"
-                    + "name varchar(200),"
-                    + "email varchar(200),"
-                    + "mobilenumber varchar(10),"
-                    + "address varchar(200),"
-                    + "password varchar(200),"
-                    + "security varchar(200), "
-                    + "answer varchar(200),"
-                    + " status varchar(20),"
-                    + "UNIQUE(email)";
+            String userTable="create table user(id int AUTO_INCREMENT primary key,name varchar(200),email varchar(200),mobilenumber varchar(10),address varchar(200),password varchar(200),security varchar(200),answer varchar(200),status varchar(20),UNIQUE(email))";
+                   
+                   
             Dboperations.setDataOrDelete(userTable,"User Table Created successfully");
         }
         catch(Exception e)
