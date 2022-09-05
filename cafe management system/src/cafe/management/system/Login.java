c6da0799f3ad1c11cc011d7b51edd4db1300e6f3
Hashini99 <bhhrkumari@students.nsbm.ac.lk>
2022-09-05 18:17:04 +0530

    update login

--- cafe management system/src/cafe/management/system/Login.java
+++ cafe management system/src/cafe/management/system/Login.java
@@ -4,17 +4,30 @@
  */
 package cafe.management.system;
 
+import dao.UserDao;
+import javax.swing.JOptionPane;
+import model.User;
+
 /**
  *
  * @author Hashini
  */
 public class Login extends javax.swing.JFrame {
+    public String emailPattern="^[a-zA-Z0-9]+[@]+[a-zA-Z0-9]+[.]+[a-zA-Z0-9]+$";
 
     /**
      * Creates new form Login
      */
     public Login() {
         initComponents();
+        btnlogin.setEnabled(false);
+    }
+    public void clear()
+    {
+        txtemail.setText("");
+        txtpassword.setText("");
+        btnlogin.setEnabled(false);
+        
     }
 
     /**
@@ -26,21 +39,123 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtemail = new javax.swing.JTextField();
+        txtpassword = new javax.swing.JPasswordField();
+        btnlogin = new javax.swing.JButton();
+        btnclear = new javax.swing.JButton();
+        btnexit = new javax.swing.JButton();
+        btnforgot = new javax.swing.JButton();
+        btnsignup = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel1.setText("Login");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 210, 150, 40));
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setText("Email");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 300, -1, -1));
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setText("Password");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 340, -1, -1));
+
+        txtemail.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        getContentPane().add(txtemail, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 300, 163, -1));
+
+        txtpassword.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        getContentPane().add(txtpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 330, 163, -1));
+
+        btnlogin.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnlogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/login.png"))); // NOI18N
+        btnlogin.setText("Login");
+        btnlogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnloginActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnlogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(545, 420, 110, -1));
+
+        btnclear.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnclear.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/clear.png"))); // NOI18N
+        btnclear.setText("Clear");
+        btnclear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnclearActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnclear, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 420, 107, -1));
+
+        btnexit.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnexit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/exit small.png"))); // NOI18N
+        btnexit.setText("Exit");
+        btnexit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnexitActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnexit, new org.netbeans.lib.awtextra.AbsoluteConstraints(890, 420, -1, -1));
+
+        btnforgot.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnforgot.setText("Forgot password");
+        getContentPane().add(btnforgot, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 490, -1, -1));
+
+        btnsignup.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnsignup.setText("Signup");
+        getContentPane().add(btnsignup, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 490, 100, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/first page background.PNG"))); // NOI18N
+        jLabel4.setText("jLabel4");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(-30, -20, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnloginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnloginActionPerformed
+        // TODO add your handling code here:
+        String email=txtemail.getText();
+        String password=txtpassword.getText();
+        User user=null;
+        user= UserDao.login(email, password);
+        if (user==null)
+        {
+           JOptionPane.showMessageDialog(null, "<html><b style=\"color:red\"> Incorrect user name or password</b></html>","Message",JOptionPane.ERROR_MESSAGE);
+        else
+        {
+                   if(user.getStatus().equals("false"))
+                {
+                  
+                   JOptionPane.showMessageDialog(null,email);
+                clear();
+                   }
+                if(user.getStatus().equals("true"))
+                {
+                setVisible(false);
+                new Home(email).setVisible(true);
+                }
+                   }
+        }
+    
+    }//GEN-LAST:event_btnloginActionPerformed
+
+    private void btnclearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnclearActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnclearActionPerformed
+
+    private void btnexitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnexitActionPerformed
+        // TODO add your handling code here:
+        int a=JOptionPane.showConfirmDialog(null, "Do you really want to exit","Select",JOptionPane.YES_NO_OPTION);
+     if (a==0){
+         System.exit(0);
+     }
+    }//GEN-LAST:event_btnexitActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +193,17 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnclear;
+    private javax.swing.JButton btnexit;
+    private javax.swing.JButton btnforgot;
+    private javax.swing.JButton btnlogin;
+    private javax.swing.JButton btnsignup;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField txtemail;
+    private javax.swing.JPasswordField txtpassword;
     // End of variables declaration//GEN-END:variables
 }
